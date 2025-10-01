343a54775fa66c2934c5a3e379119ae211aa31f4
lunhect <hlunamoreno17@gmail.com>
2025-10-01 14:14:44 +0200

    Update DatosUsuario.java

--- src/utilidades/DatosUsuario.java
+++ src/utilidades/DatosUsuario.java
@@ -1,38 +1,41 @@
 package utilidades;
 
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class DatosUsuario {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        
         System.out.print("Introduce tu nombre: ");
         String nombre = sc.nextLine();
+
+        
+        if (nombre.length() <= 2 || nombre.length() > 20) {
+            System.out.println("ERROR: nombre no válido.");
+            sc.close();
           
-      
+        }
+        
+        
+        
+
+        
         System.out.print("Introduce tu edad: ");
         int edad = sc.nextInt();
+       
 
-
-        if (edad<=0){
-    System.out.println("ERROR");
-} else {
-    if (edad>120)
-    System.out.println("ERROR");
-}
- 
-}
-
+        if (edad <= 0 || edad > 120) {
+            System.out.println("ERROR: Edad no válida.");
+            sc.close();
+          
+        }
 
         System.out.print("Introduce tu correo electrónico: ");
         String email = sc.nextLine();
 
-        
         boolean emailValido = ProcesadorTexto.esEmailValido(email);
 
         if (emailValido) {
-           
             Usuario usuario = new Usuario(nombre, edad, email);
             usuario.mostrarInformacion();
         } else {
